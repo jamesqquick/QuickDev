9ff4bd7e29385ef35ebac66c894a03cd51886943
James Quick <james.q.quick@outlook.com>
2015-12-15 16:27:46 -0500

    Added Windows 10 Project

---
+++ XamarinPart2/XamarinPart2Windows/TextToSpeechWindows.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Media.SpeechSynthesis;
+using XamarinPart2;
+using XamarinPart2Windows;
+using Windows.UI.Xaml.Controls;
+
+[assembly: Xamarin.Forms.Dependency(typeof(TextToSpeechWindows))]
+namespace XamarinPart2Windows
+{
+	class TextToSpeechWindows : ITextToSpeech
+	{
+		public async void Speak(string text)
+		{
+			SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+			SpeechSynthesisStream stream = await synthesizer.SynthesizeTextToStreamAsync(text);
+			MediaElement media = new MediaElement();
+			media.AutoPlay = true;
+			media.SetSource(stream, stream.ContentType);
+			media.Play();
+
+
+		}
+	}
+}
